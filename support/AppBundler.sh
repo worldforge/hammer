b2e0ca1b0351e56a2644da1c7f0519dede48dd96
Peter Szücs <peter.szucs.dev@gmail.com>
2014-08-01 15:25:06 +0200

    Add support for bundling Android apk files.

--- support/AppBundler.sh
+++ support/AppBundler.sh
@@ -6,11 +6,156 @@
 
 set -e
 
-export HAMMERDIR=$PWD
-export WORKDIR=$HAMMERDIR/work
-export PREFIX=$WORKDIR/local
-export DEPS_SOURCE=$WORKDIR/build/deps
-export MAKEOPTS="-j3"
+if [[ x"$HAMMERDIR" = x"" ]] ; then
+  echo "This script should be called through hammer.sh or with a configured environment with setup_env.sh"
+fi
+
+if [ $TARGET_OS = "android" ] ; then
+
+# I couldn't figure out how to extract dependencies using libtool, so I have written a script, which will do this for me.
+function getLibToolDeps()
+{  
+  # get the dependencies of the *.la input file
+  local CONTENT=`sed -n "s/dependency_libs='\\(.*\\)'/\\1/p" < $1`
+  
+  # list dependent la files
+  local LA_FILES="`printf '%s\n' "$CONTENT" | grep -oh '[^[:blank:]]*\.la'`"
+  
+  # We want to replace each la file with its own dependencies recursively.
+  # NOTE: The order is important! The content of the la file needs to be inserted at the la location.
+  for LA_FILE in $LA_FILES
+  do
+    local LA_REAL_FILE="$LA_FILE"
+    if [ ! -f $LA_REAL_FILE ] ; then
+      local LA_REAL_FILE=${LA_REAL_FILE#"="}
+    fi
+    local LA_CONTENT="`getLibToolDeps $LA_REAL_FILE`"
+    local LA_CONTENT_ESCAPED=$(printf '%s\n' "$LA_CONTENT" | sed 's/[\&/]/\\&/g')
+    local LA_FILE_ESCAPED=$(printf '%s\n' "$LA_FILE" | sed 's/[[\.*^$(){}?+|/]/\\&/g')
+    local CONTENT=`printf '%s\n' "$CONTENT" | sed -r "s/$LA_FILE_ESCAPED/$LA_CONTENT_ESCAPED/g"`
+  done
+  
+  # get the static lib of the la. "$PREFIX/lib/EmberAndroid.la" ==> "-L$PREFIX/lib -lEmberAndroid"
+  local SELF_A=`sed -n "s/old_library='lib\\(.*\\).a'/\\1/p" < $1`
+  if [ x"$SELF_A" = x"" ] ; then
+      echo "failed to determine libname for $1"
+  fi
+  local CONTENT="-L`dirname $1` -l${SELF_A} $CONTENT"
+  printf '%s\n' "$CONTENT"
+}
+function removeDuplicateLinkFlags()
+{
+  # warning: This function may not work with --start-group or some other order dependent linker flags!
+  CONTENT=" $1 "
+  # Replace tabs with spaces to make other steps easier.
+  CONTENT=$(printf "%s\n" "$CONTENT" | sed -e "s/[[:blank:]]/ /g" )
+  #We want to keep first -L* instances and last -l* instances
+  LIBDIRS="`printf '%s\n' "$CONTENT" | grep -oh '\-L[^ ]*'`"
+  for LIBDIR in $LIBDIRS
+  do
+    LIBDIR_ESCAPED=$(printf '%s\n' "$LIBDIR" | sed 's/[[\.*^$(){}?+|/]/\\&/g')
+    CONTENT=$(printf "%s\n" "$CONTENT" | sed -e "s/ $LIBDIR_ESCAPED / _$LIBDIR_ESCAPED /1" -e "s/ $LIBDIR_ESCAPED / /g" -e "s/ _\($LIBDIR_ESCAPED\) / \1 /" )
+  done
+  
+  LIBS="`printf '%s\n' "$CONTENT" | grep -oh '\-l[^ ]*'`"
+
+  for LIB in $LIBS
+  do
+    LIB_ESCAPED=$(printf '%s\n' "$LIB" | sed 's/[[\.*^$(){}?+|/]/\\&/g')
+    CONTENT=$(printf "%s\n" "$CONTENT" | sed -e "s/\\(.*\\) $LIB_ESCAPED /\1 _$LIB_ESCAPED /g" -e "s/ $LIB_ESCAPED / /g" -e "s/ _\($LIB_ESCAPED\) / \1 /" )
+  done
+  # Replace multiple spaces, -lpthread and -lGLESv1_CM, which are hardcoded dependencies in some libs.
+  CONTENT=$(printf "%s\n" "$CONTENT" | sed -e "s/ \\-lGLESv1_CM / /g" -e "s/ \\-lpthread / /g" -e "s/ \\-l / /g" -e "s/  */ /g" )
+  echo "$CONTENT"
+}
+
+  LINKFLAGS="`getLibToolDeps $PREFIX/lib/libEmberAndroid.la`"
+  LINKFLAGS="`removeDuplicateLinkFlags \"$LINKFLAGS\"`"
+  
+  if [ x"$TARGET_ARCH" = x"x86" ] ; then
+    ABI="x86"
+    STL_PATH=$ANDROID_NDK/sources/cxx-stl/gnu-libstdc++/4.8/libs/$ABI
+  elif  [ x"$TARGET_ARCH" = x"ARMv7" ] ; then
+    ABI="armeabi-v7a"
+    STL_PATH=$ANDROID_NDK/sources/cxx-stl/gnu-libstdc++/4.8/libs/$ABI/thumb
+  else
+    echo "Undefined ABI for target architecture ${TARGET_ARCH}!"
+    exit 1
+  fi
+  
+  # The android-ndk will strip the c++ runtime to the sources that it owns. However it ignores the C++ needs of dependencies in LOCAL_LDLIBS. (bug in ndk)
+  # It could be fixed by adding volatile std::sring and std::stringstream, etc to the EmberJNI.cpp, but that would be ugly.
+  # To not mess with multiple C++ runtimes it will remove -lstdc++ from LOCAL_LDLIBS too.
+  # So we will add the libgnustl_static.a with full path.
+  LINKFLAGS="$LINKFLAGS -L$STL_PATH -lgnustl_static"
+  
+  # some missing ogre deps.
+  echo $LINKFLAGS
+  LINKFLAGS_ESCAPED=$(printf '%s\n' "$LINKFLAGS" | sed 's/[\&/]/\\&/g')
+  SOURCE_ESCAPED=$(printf '%s\n' "$SOURCE" | sed 's/[\&/]/\\&/g')
+  PREFIX_ESCAPED=$(printf '%s\n' "$PREFIX" | sed 's/[\&/]/\\&/g')
+  
+  EMBER_APP="$WORKDIR/ember.app"
+  PROJECT_DIR="$BUILD/clients/ember_apk/$BUILDDIR"
+  PROJECT_SOURCE_DIR="$SOURCE/clients/ember/src/extensions/android/project"
+  KEYSTORE="$PROJECT_DIR/my-release-key.keystore"
+
+  # From now on we only need the PATH from the environment (location of 'android' and 'ndk-build'). Change condition if you want to let NDK decide build flags.
+
+  
+  #if [ ! -d $PROJECT_DIR ]; then
+    mkdir -p $PROJECT_DIR
+    cp -r $PROJECT_SOURCE_DIR/* $PROJECT_DIR
+    cp $PROJECT_DIR/jni/Android.mk.in $PROJECT_DIR/jni/Android.mk
+    cp $PROJECT_DIR/jni/Application.mk.in $PROJECT_DIR/jni/Application.mk
+    sed -i '1i# This is a generated file! Please edit Android.mk.in, then run AppBundler to regenerate.' $PROJECT_DIR/jni/Android.mk
+    sed -i '1i# This is a generated file! Please edit Application.mk.in, then run AppBundler to regenerate.' $PROJECT_DIR/jni/Application.mk
+    sed -i -e "s/%EXTRA_LIBS%/$LINKFLAGS_ESCAPED/g" -e "s/%SOURCE%/$SOURCE_ESCAPED/g" -e "s/%PREFIX%/$PREFIX_ESCAPED/g" -e "s/%SYSROOT%/$SYSROOT_ESCAPED/g" $PROJECT_DIR/jni/Android.mk
+    sed -i -e "s/%ABI%/$ABI/g" $PROJECT_DIR/jni/Application.mk
+  #fi
+  export EMBER_APP="$WORKDIR/ember.apk"
+  
+  if [ "1" = "1" ] ; then
+    export PATH_SAVE="$PATH"
+    export DEBUG_BUILD_SAVE="$DEBUG_BUILD"
+    eval `$SUPPORTDIR/setup_env.sh pop_env`
+    export PATH="$PATH_SAVE"
+    export DEBUG_BUILD="$DEBUG_BUILD_SAVE"
+  fi
+  
+  android update project \
+    --target "android-15" \
+    --name "Ember" \
+    --path "$PROJECT_DIR"
+    
+  if [ "$DEBUG_BUILD" = "0" ]; then
+    echo Building release binaries!
+    cd $PROJECT_DIR/jni
+    ndk-build -B
+    cd $PROJECT_DIR
+    ant release
+    if [ ! -f $KEYSTORE ]; then
+      # You should change the password!
+      keytool -genkey -v -keystore my-release-key.keystore -alias ember -keyalg RSA \
+      -dname "CN=mqttserver.ibm.com, OU=ID, O=IBM, L=Hursley, S=Hants, C=GB" \
+      -keysize 2048 -validity 10000 -storepass unofficialkeystore -keypass unofficialkey
+    fi
+    jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore \
+    -storepass unofficialkeystore -keypass unofficialkey bin/Ember-release-unsigned.apk ember
+    mv -f bin/Ember-release-unsigned.apk bin/Ember-release.apk
+    #adb install bin/Ember-release.apk
+  else
+    echo Building debug binaries!
+    cd $PROJECT_DIR/jni
+    ndk-build NDK_DEBUG=1 -B
+    cd $PROJECT_DIR
+    ant debug
+  fi
+
+  exit 0
+fi
+
+
 export EMBER_APP="$WORKDIR/ember.app"
 export WEBEMBER_PLUGIN="$WORKDIR/WebEmber.plugin"
 export DYLIBBUNDLER="$PREFIX/bin/dylibbundler"
